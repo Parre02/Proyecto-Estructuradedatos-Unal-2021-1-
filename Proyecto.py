cfa9310e78dd6ee234120ea1ac7a6e83257b6735
Parre02 <moneco3000@hotmail.com>
2022-04-16 18:10:29 -0500

    Se agrega el color de las cartas, con el mensaje print

--- Proyecto.py
+++ Proyecto.py
@@ -61,7 +61,7 @@ if(entradan1 == 1):
     Torre_D = deque()
 
 
-    print(f'El juego se inicializa \n Columnas 1-7 \n Columna 1 {b[-1]} Tiene {len(b)} cartas \n Columna 2 {c[-1]} Tiene {len(c)} cartas \n Columna 3 {d[-1]} Tiene {len(d)} cartas \n Columna 4 {e[-1]} Tiene {len(e)} cartas \n Columna 5 {f[-1]} Tiene {len(f)} cartas \n Columna 6 {g[-1]} Tiene {len(g)} cartas \n Columna 7 {h[-1]} Tiene {len(h)} cartas \n\n En las torres de figuras (P,C,T,D) no hay cartas \n\n En la cola de arraste no hay cartas destapadas')
+    print(f'El juego se inicializa \n Columnas 1-7 \n Columna 1 {b[-1]} Tiene {len(b)} cartas \n Columna 2 {c[-1]} Tiene {len(c)} cartas \n Columna 3 {d[-1]} Tiene {len(d)} cartas \n Columna 4 {e[-1]} Tiene {len(e)} cartas \n Columna 5 {f[-1]} Tiene {len(f)} cartas \n Columna 6 {g[-1]} Tiene {len(g)} cartas \n Columna 7 {h[-1]} Tiene {len(h)} cartas \n\n En las torres de figuras (P,C,T,D) no hay cartas \n\n En la cola de arraste no hay cartas destapadas \n\n Las picas y treboles son de color Negro, Los corazones y diamentes son de color Rojo' )
 
     print(a)
 
@@ -73,18 +73,26 @@ elif(entradan1 == 2):
 
 
 Varialbe_controlsalida = False
-#while(entradan1 == 1):
-    #entradan1 == 2
-    #if(entradan1 == 2):
-     #   break
-    ## FUNCIONES A EJECUTAR 
+while(entradan1 == 1 and variable_con == False):
+    entradan1 == 2
+    Varialbe_controlsalida = True
+    
+    # FUNCIONES A EJECUTAR 
 
 
     # Destapar cola de arraste Laura / recordar notificar si alguna accion es invalida
     # Reiniciar cola de arraste Laura
 
 
-    # 
+
+
+  
+## POR Elegir
+  #Llevar de cola de arrastre a Columna Y z
+  #Llevar de cola de arrastre a Torre de figura X (P: Pica, C: Corazón, T: Trébol, D: Diamante)
+  #Llevar de Columna Y a Torre de figura X
+  #Llevar Z cartas de la Columna X a la columna Y 
+  #LLevar de la torrre de figura Y a columna X (Ver wtts ED - 11 abril monitor hablo)
 
 
 
