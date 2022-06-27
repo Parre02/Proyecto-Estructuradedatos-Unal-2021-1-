989156e90cf2482d7119b6cd0e849197c9866d9b
Parre02 <80075880+Parre02@users.noreply.github.com>
2022-06-27 13:49:16 -0500

    Add files via upload

---
+++ proyecto_2.py
@@ -0,0 +1,280 @@
+import random
+from collections import deque
+from xmlrpc.client import Boolean
+
+print('''                                                             ..',,;;;;;,,'..
+                                                         ..',;;;;;;;;;;;;;;;;;;;,;;;:::,.
+                                                    ..';;;;;;;;;;;;;;;;;;;;;;oOOxoooooooodkx;
+                                                 .,;;;;;;;;;;;;;;;;;;;;;;;cO0lcoOXXKKKKKX0dccOd
+                                             ..,;;;;;;;;;;;;;;;;;;coxo;;lKk,lKKKKKKKKKKKKKKKKo;0l
+                                           .,;;;;;;;;;;;;;;;;cdkOkol,Nd00..KKKKKX0dc;;cdKKKKKKK:dO
+                                        .';;;;;;;;;;;;;;;;;l0k,clkKXO'Wc .XKKKKO'       .;0KKKKXcoO
+                                      .,;;;;;;;;;;:ld:;;;:0k'.',XKKKXl.  OKKKK0 ;ll:. .,;,.OKKKKX,0,
+                                    .,;;;;;;;:oxOkxolx0x:cN.';;.oKKKKX.  NKKKX:x0c:lk0,.,.  KKKKK0,X
+                                   ,;;;;:oxOkd:,ld0XXkcck0KK ,.  0KKKKK  NKKKXcxk;;;;:K: .' oKKKKN.M
+                                 .;;;:kOolllx0. 0KKKKKKXkcckd .' .XKKKKk KKKKKK.Nc;;;;:W..  oKKKKN.N
+                              ..,;;:k0;..KKKKKK .XKKKKKKKKXkc.    :XKKKX:;XKKKKk,Kd;;;;K:  .XKKKKk::
+                         .:dxxlOx;;00 ,;'.XKKKKx :XKKKKXKKKKKXk:   xKKKKX.oXKKKK0:lkOO0O. cKKKKKX.O
+                      'OxllloOK.Xl;:W,',. cXKKKX; kKKKKNdkXKKKKKXx;.KKKKK0 cXKKKKKKxlllokXKKKKKK'x 
+                    .Ox. .XKKKKk'N:;oN. .' kKKKKX. KKKKK0 .cOXKKKKKXKXKKKKd .dXKKKKKKKKKKKKKKKl'c
+                    ol    :XKKKXco0;;OO..   XKKKK0 'XKKKKd   .cOXKKKKKKKKKX,   :xKXKKKKKXX0d; o
+                     K.    xKKKKX.0d;;Nc    'XKKKKo oXKKKX,     .l0XKKKKKKKX.     .';;;,.   l
+                      0     KKKKK0.Nc;cW.    oKKKKX. 0KKKKX        .l0XKKKKKO           .c.
+                      ;o   .'XKKKKd;X;;xX     KKKKKO .XKKKKO.d'       .o0Kko:.OOxdoodxk.
+                       k' ,;.lKKKKX,xk;;Kx    dKKKKN  ;XKKKXllM0d.          cKd;;:::;'
+                        N.,;. OKKKKX.Ok;:W'   KKKKKN   xKKKKX,0kck0o.  .,lx0d;;;;;;;
+                        .k  .'.KKKKKXc:ddO. ;0KKKKKd    KXOdc.:W:;;lkOOkdc;;;;;;;;
+                         c:..  .0KKKKKXOxxOXKKKKKXd.        'OOc;;;;;;;;;;;;;;;;
+                          x.     oXKKKKKKKKKKKKKd;xW'   'cxOOc;;;;;;;;;;;;;;;'
+                           l,     .ckKXXXXX0xl,.oKolXxkOxl;;;;;;;;;;;;;;;;;
+                             x.        ...   .oKo;;;;;;;;;;;;;;;;;;;;;;;
+                               c;.       .,oOOl;;;;;;;;;;;;;;;;;;;;;
+                                    lxxkOkoc;;;;;;;;;;;;;;;;;;;.
+                                         ';;;;;;;;;;;;;;,  ''')
+
+
+##############################################################################################################
+
+# Inicializacion del programa, se reparten todas las cartas 
+
+lista_cartas = ["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "RB", "RC", "R2+",
+"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "RB", "RC", "R2+",
+ "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "AB", "AC", "A2+",
+  "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "AB", "AC", "A2+",
+   "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "VB", "VC", "V2+",
+    "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "VB", "VC", "V2+",
+     "Z1", "Z2", "Z3", "Z4", "Z5", "Z6", "Z7", "Z8", "Z9", "ZB", "ZC", "Z2+",
+      "Z1", "Z2", "Z3", "Z4", "Z5", "Z6", "Z7", "Z8", "Z9", "ZB", "ZC", "Z2+"]
+
+
+# Maso de de cartas donde esta la carta del centro
+maso = []
+
+# Cartas aleatorias
+random.shuffle(lista_cartas) # O(n*n)
+
+# Turnos
+turnos = deque()
+turno_1 = []
+turno_2 = []
+turno_3 = []
+turno_4 = []
+
+# Identificar los turnos (se va a quitar despues) 
+turno_1.append("turno_1")
+turno_2.append("turno_2")
+turno_3.append("turno_3")
+turno_4.append("turno_4")
+
+
+
+for AddCards in range(8):
+    turno_1.append(lista_cartas.pop())
+    turno_2.append(lista_cartas.pop())
+    turno_3.append(lista_cartas.pop())
+    turno_4.append(lista_cartas.pop())
+
+#Se ramdomizan los turnos para comenzar 
+
+turnos.append(turno_1)
+turnos.append(turno_2)
+turnos.append(turno_3)
+turnos.append(turno_4)
+
+random.shuffle(turnos)
+
+
+# maso agrega una carta aleatoria que es con la que se va a comenzar el juego
+maso.append(lista_cartas.pop())
+sentido = True
+
+
+########################################################################################################################
+
+
+
+# Busqueda de carta que pueda servir por turno.
+def BusquedaDeCarta(turno, cartaCentro, masoRestante):
+    encontrada = False
+    for carta in turno:
+        if carta[0] == cartaCentro[-1][0] or carta[1] == cartaCentro[-1][1]:
+            cartaCentro.append(carta)
+            turno.remove(carta)
+            encontrada = True
+            return 1
+    if encontrada == False and masoRestante[-1][0] == cartaCentro[-1][0] or masoRestante[-1][1] == cartaCentro[-1][1]:
+        cartaCentro.append(masoRestante.pop())
+        return 1
+    else:
+        turno.append(masoRestante.pop())
+        return -1
+
+# Funcion de cartas especiales
+# - C = cambio de sentido
+# - B = Bloqueo
+# - 2 = sumar dos cartas al siguiente
+
+def CartaEspecial(cartaCentro, turnos):
+    global sentido
+    # B en este caso es para identificar si la carta es especial osea si contiene (C, B, 2)
+    # Cambio de sentido
+    B = cartaCentro[-1]
+    if B[1] == 'C' and sentido == True: #Sentido horario
+        sentido = False
+    elif B[1] == 'C' and sentido == False: #Sentido Antihorario
+        sentido = True
+    
+    # Bloqueo
+
+    # +2
+
+
+##En esta funcion se verifica si el jugador puede arrastrar
+def verificarArrastre (masoCartasJugador, MasoCentro):
+    posiblejugadas = []
+
+    for i in range(len(masoCartasJugador)):
+        if MasoCentro[-1][0] == masoCartasJugador[i][0]:
+            posiblejugadas.append(masoCartasJugador[i])
+        elif MasoCentro[-1][1] == masoCartasJugador[i][1]:
+            posiblejugadas.append(masoCartasJugador[i])
+    if len(posiblejugadas) != 0:
+        return False
+    else:
+        return True
+
+def verificarCartaTirar (masoCartasJugador, masoCentro,cartaTirar):
+    ##Se verifica si existe la carta que deseo tirar en el maso y se realizan las comparaciones para tirar
+    posiblejugadas = []
+
+    if cartaTirar in masoCartasJugador:
+        print("La carta esta en el maso")
+        
+        for i in range(len(masoCartasJugador)):
+            if masoCentro[-1][0] == masoCartasJugador[i][0]:
+                posiblejugadas.append(masoCartasJugador(i))
+            elif masoCentro[-1][1] == masoCartasJugador[i][1]:
+                posiblejugadas.append(masoCartasJugador(i))
+        return posiblejugadas
+
+    else:
+        print("La carta no esta en el maso, favor coloque bien el dato")
+        
+
+
+
+
+
+
+while True:
+    # Si el maso de cartas restantes se queda en 0 este se reemplaza por el maso de cartas que no estas en posesion de ningun jugador
+    if len(lista_cartas) == 0:
+        Centro = maso.pop()
+        lista_cartas = list(maso)
+        random.shuffle(lista_cartas)
+        maso = []
+        maso.append(Centro)
+    
+    verificarSiSeArrastro = False
+    verificarPosibleJugada = False
+
+    while True:
+        if turnos[0][0] == "turno_1":
+            print("Menu del jugador")
+            print("Opciones:")
+            print(" 1.Ver Cartas en mi maso")
+            print(" 2.Arrastrar")
+            print(" 3.Ver la carta del centro")
+            print(" 4.Tirar carta")
+
+            entrada = int(input("Eliga la opcion con un entero: "))
+
+
+            if entrada == 1:
+                listaImpresionCartas = turnos[0] 
+                print(*listaImpresionCartas)
+            elif entrada == 2:
+                verificarPosibleArrastre = False
+                verificarPosibleArrastre = verificarArrastre(turnos[0],maso)
+
+                if verificarPosibleArrastre == True and verificarSiSeArrastro == False:
+                    print("Se ha arrastrado una carta")
+                    turnos[0].add(lista_cartas.pop(-1))
+                    verificarSiSeArrastro = True
+                else:
+                    print("Aun no se puede arrastrar tienes movimiento posibles o ya arrastraste")
+            elif entrada == 3:
+                print(' CARTAS DEL CENTRO ')
+                print(" ")
+                print(" ")
+                print(maso[-1])
+                print(" ")
+                print(" ")
+            elif entrada == 4:
+
+                ##Aqui da errores revisar aun falta hacer el filtro mejor
+                print("\nImportante las Cartas que empiezan con Z al comienzo son las azules")
+                print("Las cartas que por ejemplo son RB, RC, R2+ corresponden a la primera letra el color R = Rojo y en la segunda letra \n B = Cancelar el proximo jugador \n C = Cambio de Sentido \n 2+ es igual a +2")
+                print("\nQue carta desea tirar?")
+                varCartaTirar = str(input())
+                masoCartasJugador = turnos[0]
+                
+                varPosibleJugadas = verificarCartaTirar(masoCartasJugador,maso,varCartaTirar)
+
+                if len(varPosibleJugadas) != 0:
+                    verificarPosibleJugada = True
+
+                if verificarPosibleJugada == False:
+                    print("Movimiento Invalido")
+                else:
+                    print("Se ha tirado la carta al centro")
+
+                    for i in range(turnos[0]):
+                        if varCartaTirar == turnos[0][i]:
+                            del(turnos[0][i])
+                            break
+                    break
+        else:
+            break
+
+
+
+
+    print("Turno de la maquina")
+    BusquedaDeCarta(turnos[0], maso, lista_cartas)
+    CartaEspecial(maso, turnos)
+
+
+
+
+
+   
+
+
+
+    
+
+    if len(turnos[0]) == 0:
+        break
+
+
+    for k in turnos:
+        print(k)
+
+    print(" ")
+
+    print(maso)
+    print(sentido)
+    print(lista_cartas)
+    if sentido == True:
+        turnos.append(turnos.popleft())
+    if sentido == False:
+        turnos.appendleft(turnos.pop())
+
+    ##Modificar esto ya que estoy haciendo la comparacion para acceder al indice por medio de del indice 0 y se puede generar error cuando se hace un cambio de sentido ya que la comparacion para pasar si es turno del jugador es con la poscion 0 de el deque turnos
+
+# Faltante
+# JOSE
+# - Cartas especiales: +2, bloqueo
+# SIMON
+# - Automatizar turnos y randomizar
